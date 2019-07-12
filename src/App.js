bbf8b847c3acfc6eed3b34c3a970bdd101b4cc8b
Ariel Calisaya <51676628+ArielCalisaya@users.noreply.github.com>
2019-07-12 17:00:05 -0400

    ExcuseGenerator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+window.onload = () =>{
+  document.querySelector("#button").addEventListener('click', () =>{
+    document.querySelector("#excuse").innerHTML = excuseGenerator();
+  });
+};
 
-export default App;
+let excuseGenerator = () =>{
+  let who = ['Ariel','The dog', 'My granma','Byron', 'His turtle','Diego', 'My bird'];
+  let what = ['eat', 'pissed', 'crushed', 'broked'];
+  let when = ['before the class', 'right in time', 'when I finished', 'during my lunch', 'while I was praying'];
+
+  let whoPlus = Math.floor(Math.random() * who.length);
+  let whatPlus= Math.floor(Math.random() * what.length);
+  let whenPlus= Math.floor(Math.random() * when.length);
+
+  return who[whoPlus] + ' '+ what[whatPlus]+ ' ' + when[whenPlus];
+}
+console.log("holaaa")
+
+export default excuseGenerator;
